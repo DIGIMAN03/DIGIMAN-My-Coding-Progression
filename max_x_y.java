9895e11376d126f7086d626d01a12edfd722b2fe
Michael Helfand <44706182+DIGIMAN03@users.noreply.github.com>
2025-01-26 12:27:56 -0800

    Add files via upload
    These are all projects I've done throughout high school for class or practice. Although initially trained in Java, I have since translated the skills learned in these projects to C++ and, more importantly, Python.
    
    I apologize for the lack of comments for most of the code. Hopefully, the projects are understandable enough based on their titles, except for those with Hebrew or transliterated Hebrew titles that were made for my classes.

---
+++ max_x_y.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class max_x_y {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        int x;
+        int y;
+        int max;
+
+        System.out.println("give number for a");
+        x=input.nextInt();
+        System.out.println("give number for b");
+        y=input.nextInt();
+        
+    }
+}
